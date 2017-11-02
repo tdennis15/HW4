e65a0f8a1fa1a2305070e0d0dc07403ca09ccc63
TJ Dennis <tdennis15@mail.wou.edu>
2017-11-02 15:07:04 -0700

    added readme and tut files

---
+++ Homework4/Homework4/Controllers/HW4Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Homework4.Controllers
+{
+    public class HW4Controller : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
---
+++ index.md
@@ -0,0 +1,8 @@
+---
+title: HW 4 Travis (TJ) Dennis
+layout: default
+---
+## CS 460 H4 ASP.NET MVC web page
+This project is to create a web application that will have a few navigation pages and a form in which a user provides data and it returns either a table or a list. The complexity needs to be like that of a bank loan pay out table - Amortization table.
+
+To start I went through the basic documentation that MicroSoft had available on [ASP.NET MVC structure](https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-controller).
